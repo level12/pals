3372ef0f5e561ed9a8090aacde18460fa2216f5f
Randy Syring <randy@thesyrings.us>
2018-09-21 22:13:28 -0400

    Add hang.py script
    Used to help demonstrate how postgresql will close connections.

---
+++ scripts/hang.py
@@ -0,0 +1,34 @@
+"""
+    If you run this file and monitor connections on your PostgreSQL server, you should see a backend
+    process dedicated to this lock:
+
+        select
+            stat.datname
+            , stat.pid
+            , stat.usename
+            , stat.application_name
+            , stat.state
+            , locks.mode as lockmode
+            , stat.query
+        from pg_stat_activity stat
+        left join pg_locks locks
+            on locks.pid = stat.pid
+        where locktype = 'advisory'
+
+    Then kill the Python process running this file in any way you want and make sure the PostgreSQL
+    pid with the lock goes away:
+
+    1) press any key and exit
+    2) CTRL-C
+    3) kill -9 <pid>
+"""
+import os
+
+import plocks
+
+locker = plocks.Locker('plocker-hang', 'postgresql://postgres:password@localhost:54321/vacbal')
+
+lock = locker.lock('hang')
+lock.aquire()
+print('My pid is: ', os.getpid())
+input('Lock aquired, press any key to exit: ')
